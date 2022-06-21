e3fc0ac9efa580a83213a0609b68744007d1e3c3
surya489 <98193638+surya489@users.noreply.github.com>
2022-06-21 10:03:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+//  product details  //
+
+var mainimg = document.getElementById("main_img");
+var smallimg = document.getElementById("small-img");
+
+smallimg[0].onclick = function(){
+    mainimg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    mainimg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    mainimg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    mainimg.src = smallimg[3].src;
+}
